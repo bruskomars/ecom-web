d294fd4751c32ed9036480bb840d314147ed1e39
Mars Alexis Regidor <pythonista.mars@gmail.com>
2024-01-29 22:10:44 +0800

    first commit

---
+++ shop/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Product
+from django.core.paginator import Paginator
+
+# Create your views here.
+def home(request):
+    products = Product.objects.all()
+
+    search = request.GET.get('search')
+    if search != '' and search is not None:
+        products = Product.objects.filter(name__icontains=search)
+
+
+    ############ PAGINATION ############
+    paginator = Paginator(products, 4)  # USE OF PAGINATOR
+    page = request.GET.get('page')
+
+    products = paginator.get_page(page)
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'shop/home.html', context)
