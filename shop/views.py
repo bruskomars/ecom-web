f3d9f0a9143b3ab7228cc3a4e294674f08592807
Mars Alexis Regidor <pythonista.mars@gmail.com>
2024-01-30 23:00:58 +0800

    navbar, popup , detail view

--- shop/views.py
+++ shop/views.py
@@ -21,3 +21,11 @@ def home(request):
         'products': products,
     }
     return render(request, 'shop/home.html', context)
+
+def detail(request, pk):
+    product_item = Product.objects.get(id=pk)
+    context = {
+        'product_item': product_item,
+    }
+
+    return render(request, 'shop/detail.html', context)
